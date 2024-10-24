d15aa7084e32810cc5d58d48656520401da59234
altrolucky <altrolucky@HOME-PC>
2024-10-24 21:06:23 +0300

    Add project files.

---
+++ MASIV/MASIV.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <random>
+using namespace std;
+
+int main()
+
+{
+
+    int oragutan[5]{ 1,2,3,4,5 };
+
+    for (int i = 4; i > -1; i--)
+    {
+        cout << oragutan[i] << ", ";
+    }
+}
